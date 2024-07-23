7e38b20adb88a3db9d727e62ec1bc38f895182e4
Idaraobong James <iydeejaymes@gmail.com>
2024-07-23 11:49:03 +0100

    Hierarchy setting

---
+++ school-padi/src/config/routes.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+import Landingpage from '../pages/landingpage';
+import Navbar from '../components/Landingpage/Navbar';
+import Footer from '../components/Landingpage/Footer';
+
+function PageRoutes() {
+     return (
+       <div>
+         <Navbar /> {/* Ensure Navbar is always visible */}
+         <Routes>
+           <Route path="/" element={<Landingpage />} />
+           <Route path="/navbar" element={<Navbar />} />
+        <Route path="/footer" element={<Footer />} />
+           </Routes>
+      <Footer /> {/* Ensure Footer is always visible */}
+    </div>
+  );
+}
+
+export default PageRoutes;
---
+++ school-padi/src/pages/landingpage.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/Landingpage/Navbar";
+import Footer from "../components/Landingpage/Footer";
+
+function App() {
+     return (
+       <div className="max-w-[100vw]  overflow-hidden">
+       </div>
+  );
+}
+
+export default App;   
