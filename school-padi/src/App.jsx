d71831229f35d4643f3d84375fad9fde3bf5b321
Idaraobong James <iydeejaymes@gmail.com>
2024-07-23 10:49:57 +0100

    Setup

--- school-padi/src/App.jsx
+++ school-padi/src/App.jsx
@@ -1,13 +1,15 @@
-
 function App() {
-
   return (
- 
-      <div className="App">
-      </div>
-        
-  
-  )
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Welcome to My React App</h1>
+      <p className="mt-4 text-lg text-gray-700">
+        Tailwind CSS is working!
+      </p>
+      <button className="px-4 py-2 mt-6 text-white bg-blue-500 rounded hover:bg-blue-600">
+        Click Me
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
