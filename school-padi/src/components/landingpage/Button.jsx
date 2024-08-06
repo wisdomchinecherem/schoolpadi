946e76275bbe5f8839bb63bdda28ce048525d0d7
Idaraobong James <iydeejaymes@gmail.com>
2024-08-06 07:07:23 +0100

    Create Button.jsx

---
+++ school-padi/src/components/landingpage/Button.jsx
@@ -0,0 +1,32 @@
+
+const Button = ({
+  bgColor = "#FFFFFF",  // Default background color: White
+  color = "#8E959C",    // Default text color: Grey
+  borderColor = "#01356B", // Default border color: Medium Dark Blue
+  width,
+  height = "40px",
+  fontSize = "15px",
+  fontWeight = "700",
+  children,
+  ...rest
+}) => {
+  return (
+    <button
+      style={{
+        backgroundColor: bgColor,
+        borderColor: borderColor,
+        width: width,
+        height: height,
+        color: color,
+        fontSize: fontSize,
+        fontWeight: fontWeight,
+      }}
+      className="border text-sm font-bold p-2 rounded-sm"
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
