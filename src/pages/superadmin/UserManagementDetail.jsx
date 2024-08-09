49b3e06b422ff83162213e0f5560ea9e50e6c2b0
Beemhuse <brightawah94@gmail.com>
2024-08-09 03:29:14 -0700

    fix: superadmins

--- src/pages/superadmin/UserManagementDetail.jsx
+++ src/pages/superadmin/UserManagementDetail.jsx
@@ -47,8 +47,6 @@ export default function UserManagementDetail() {
 
         {renderTabContent()}
 
-        {/* SchoolListTable component goes here */}
-        {/* <SchoolListTable data={filteredData} /> */}
       </div>
     </section>
   )
