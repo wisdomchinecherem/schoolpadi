9b75b535c28de905680a54e5ef3412e08bac8234
Idaraobong James <iydeejaymes@gmail.com>
2024-08-10 01:50:44 +0100

    Forms added

--- src/components/adminDashboard/InfomationSystem.jsx
+++ src/components/adminDashboard/InfomationSystem.jsx
@@ -1,10 +1,23 @@
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus, faChevronDown, faBars  } from '@fortawesome/free-solid-svg-icons';
-
+import { FaExclamation, FaTrashAlt, FaLock, FaSave } from 'react-icons/fa';
 
 const InfomationSystem = () => {
 
+
+  const data = [
+    { name: 'John Doe', section: 'A', class: '10', id: '12345', email: 'john@example.com', contacts: '123-456-7890' },
+    { name: 'Jane Smith', section: 'B', class: '9', id: '67890', email: 'jane@example.com', contacts: '987-654-3210' },
+    { name: 'Jane Smith', section: 'B', class: '9', id: '67890', email: 'jane@example.com', contacts: '987-654-3210' },
+    { name: 'Jane Smith', section: 'B', class: '9', id: '67890', email: 'jane@example.com', contacts: '987-654-3210' },
+    { name: 'Jane Smith', section: 'B', class: '9', id: '67890', email: 'jane@example.com', contacts: '987-654-3210' },
+    { name: 'Jane Smith', section: 'B', class: '9', id: '67890', email: 'jane@example.com', contacts: '987-654-3210' },
+    { name: 'Jane Smith', section: 'B', class: '9', id: '67890', email: 'jane@example.com', contacts: '987-654-3210' },
+    { name: 'Jane Smith', section: 'B', class: '9', id: '67890', email: 'jane@example.com', contacts: '987-654-3210' },
+    { name: 'Jane Smith', section: 'B', class: '9', id: '67890', email: 'jane@example.com', contacts: '987-654-3210' },
+    //random data here...
+  ];
 
   const dropdownData = [
     { heading: 'Academic Session', label: 'Select Academic Session', options: ['2023/2024', '2022/2023', '2021/2022'] },
@@ -19,14 +32,14 @@ const InfomationSystem = () => {
     setOpenIndex(openIndex === index ? null : index);
   };
 
-  return (
 
+  return (
     <>
 
 {/* New Section: Student List Header */}
 <div className="bg-[#014F9E] p-2 flex justify-between items-center px-4 sm:p-4 sm:px-6">
-  <span className="text-white text-lg font-semibold leading-none sm:text-xl mt-[-4px]">Student List</span> 
-  <button className="bg-white text-[#014F9E] font-bold py-1 px-2 rounded flex items-center text-sm leading-none sm:py-2 sm:px-4 sm:text-base mt-[-4px]"> {/* Adjusted margin-top */}
+  <span className="text-white text-lg leading-none sm:text-xl mt-[-4px]">Student List</span> 
+  <button className="bg-white text-[#014F9E] py-1 px-2 rounded flex items-center text-sm leading-none sm:py-2 sm:px-4 sm:text-base mt-[-4px]"> {/* Adjusted margin-top */}
     <FontAwesomeIcon icon={faPlus} className="mr-1 sm:mr-2" />
     Add Student
   </button>
@@ -39,26 +52,26 @@ const InfomationSystem = () => {
   <div className="bg-white rounded-lg shadow-lg mx-4 py-6 px-4"> {/* Reduced vertical padding */}
     <div className="grid grid-cols-4 gap-2"> {/* Reduced grid gap */}
       <a href="/students" className="flex flex-col sm:flex-row items-center justify-center">
-        <span className="font-bold text-[#8E959C] text-xs sm:text-sm mb-1 mr-2 sm:mb-0">Students</span>
-        <div className="bg-gray-300 text-[#8E959C] rounded-full h-4 w-4 flex items-center justify-center text-[0.65rem] sm:h-6 sm:w-6 sm:text-sm">
+        <span className="text-[#8E959C] text-xs sm:text-sm mb-1 mr-2 sm:mb-0">Students</span>
+        <div className="bg-gray-300 text-[#8E959C] rounded-full h-4 w-4 flex items-center justify-center text-[0.65rem] sm:h-6 sm:w-6 sm:text-xs">
           12
         </div>
       </a>
       <a href="/teachers" className="flex flex-col sm:flex-row items-center justify-center">
-        <span className="font-bold text-[#8E959C] text-xs sm:text-sm mb-1 mr-2 sm:mb-0">Teachers</span>
-        <div className="bg-gray-300 text-[#8E959C] rounded-full h-4 w-4 flex items-center justify-center text-[0.65rem] sm:h-6 sm:w-6 sm:text-sm">
+        <span className="text-[#8E959C] text-xs sm:text-sm mb-1 mr-2 sm:mb-0">Teachers</span>
+        <div className="bg-gray-300 text-[#8E959C] rounded-full h-4 w-4 flex items-center justify-center text-[0.65rem] sm:h-6 sm:w-6 sm:text-xs">
           8
         </div>
       </a>
       <a href="/admin" className="flex flex-col sm:flex-row items-center justify-center">
-        <span className="font-bold text-[#8E959C] text-xs sm:text-sm mb-1 mr-2 sm:mb-0">Admin</span>
-        <div className="bg-gray-300 text-[#8E959C] rounded-full h-4 w-4 flex items-center justify-center text-[0.65rem] sm:h-6 sm:w-6 sm:text-sm">
+        <span className="text-[#8E959C] text-xs sm:text-sm mb-1 mr-2 sm:mb-0">Admin</span>
+        <div className="bg-gray-300 text-[#8E959C] rounded-full h-4 w-4 flex items-center justify-center text-[0.65rem] sm:h-6 sm:w-6 sm:text-xs">
           3
         </div>
       </a>
       <a href="/parents" className="flex flex-col sm:flex-row items-center justify-center">
-        <span className="font-bold text-[#8E959C] text-xs sm:text-sm mb-1  mr-2 sm:mb-0">Parents</span>
-        <div className="bg-gray-300 text-[#8E959C] rounded-full h-4 w-4 flex items-center justify-center text-[0.65rem] sm:h-6 sm:w-6 sm:text-sm">
+        <span className="text-[#8E959C] text-xs sm:text-sm mb-1  mr-2 sm:mb-0">Parents</span>
+        <div className="bg-gray-300 text-[#8E959C] rounded-full h-4 w-4 flex items-center justify-center text-[0.65rem] sm:h-6 sm:w-6 sm:text-xs">
           25
         </div>
       </a>
@@ -72,12 +85,12 @@ const InfomationSystem = () => {
 <div className="flex flex-wrap justify-center gap-8 mt-6 w-full md:px-20">
       {dropdownData.map((data, index) => (
         <div key={index} className="w-full md:w-[300px]">
-          <h2 className="text-[#101828] font-bold mb-2">{data.heading}</h2>
+          <h2 className="text-[#101828] mb-2">{data.heading}</h2>
           <div
             className="relative bg-white border w-full h-20 rounded-md flex items-center justify-between px-4 cursor-pointer"
             onClick={() => toggleDropdown(index)}
           >
-            <span className="text-[#D3DBE3] font-bold">{data.label}</span>
+            <span className="text-[#D3DBE3] ">{data.label}</span>
             <FontAwesomeIcon icon={faChevronDown} className="text-[#D3DBE3]" />
             {openIndex === index && (
               <ul className="absolute top-full left-0 right-0 bg-gray-100 border  shadow-lg z-10">
@@ -101,19 +114,54 @@ const InfomationSystem = () => {
           PlaceHolder
         </span>
         {/* Filter Card with Updated Icon */}
-        <button className="text-[#8E959C] border font-bold py-1 px-2 rounded flex items-center text-sm leading-none sm:py-2 sm:px-4 sm:text-base mt-[-4px]">
+        <button className="text-[#8E959C] border py-1 px-2 rounded flex items-center text-sm leading-none sm:py-2 sm:px-4 sm:text-base mt-[-4px]">
           <FontAwesomeIcon icon={faBars} className="mr-1 sm:mr-2" />
           Filter
         </button>
       </div>
       {/* Selected Button on the Right */}
-      <button className="text-[#8E959C] border font-bold py-1 px-2 rounded flex items-center text-sm leading-none sm:py-2 sm:px-4 sm:text-base mt-[-4px]">
+      <button className="text-[#8E959C] border py-1 px-2 rounded flex items-center text-sm leading-none sm:py-2 sm:px-4 sm:text-base mt-[-4px]">
         <FontAwesomeIcon icon={faPlus} className="mr-1 border rounded-full p-1 sm:mr-2" />
         Selected
         <FontAwesomeIcon icon={faChevronDown} className="text-[#D3DBE3] ml-2" />
       </button>
     </div>
 
+    {/* Section Form */}
+    <div className="w-full px-4 py-8">
+      <div className="w-full max-w-screen-xl bg-white shadow-lg rounded-lg border border-gray-300 overflow-hidden mx-auto">
+        <div className="bg-[#FAFBFC] p-4">
+          <div className="flex justify-between text-[#8E959C]">
+            <span>Name</span>
+            <span>Section</span>
+            <span>Class</span>
+            <span>ID</span>
+            <span>Email</span>
+            <span>Contacts</span>
+            <span>Icons</span>
+          </div>
+        </div>
+        <div className="p-4 space-y-2">
+          {data.map((item, index) => (
+            <div key={index} className="flex items-center justify-between bg-white p-2 rounded border border-gray-200 text-[#8E959C]">
+              <input type="checkbox" className="mr-4" />
+              <span>{item.name}</span>
+              <span>{item.section}</span>
+              <span>{item.class}</span>
+              <span>{item.id}</span>
+              <span>{item.email}</span>
+              <span>{item.contacts}</span>
+              <div className="flex space-x-2">
+                <FaExclamation className="text-[#8E959C] cursor-pointer" />
+                <FaTrashAlt className="text-[#8E959C] cursor-pointer" />
+                <FaLock className="text-[#8E959C] cursor-pointer" />
+                <FaSave className="text-[#8E959C] cursor-pointer" />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
     </>
     
   );
