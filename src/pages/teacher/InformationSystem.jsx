c8ed8b34228d609ca69301b41c7ea96b2f0754a8
Beemhuse <brightawah94@gmail.com>
2024-08-15 04:28:00 -0700

    fix: teachers dashboard

--- src/pages/teacher/InformationSystem.jsx
+++ src/pages/teacher/InformationSystem.jsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import { useState } from "react";
 // import { useNavigate } from "react-router-dom";
 import { TabButton } from "../../components/reusables/filters";
 import StudentsListTable from "../../components/tables/teacher/StudentsListTable";
