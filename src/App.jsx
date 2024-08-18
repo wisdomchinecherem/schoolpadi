503e9ae0a3bf67b806f946a94b68c78c15b60c23
Idaraobong James <iydeejaymes@gmail.com>
2024-08-19 00:24:53 +0100

    new

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,8 @@ import InfomationSystem from './components/informationpage/InfomationSystem';
 import Teachers from './components/informationpage/Teachers';   
 import AdminPage from './components/informationpage/AdminPage'; 
 import Parents from './components/informationpage/Parents'; 
-import Schedules from './components/schedulespage/Schedules';    
+import Schedules from './components/schedulespage/Schedules';  
+import Emails from './components/schedulespage/Emails';   
 import Graduation from './components/adminDashboard/Graduation';
 
 
@@ -58,6 +59,7 @@ const App = () => {
         <Route path="/teachers" element={<Layout><Teachers/></Layout>} /> 
         <Route path="/parents" element={<Layout><Parents/></Layout>} /> 
         <Route path="/Schedules" element={<Layout><Schedules/></Layout>} />
+        <Route path="/Emails" element={<Layout><Emails/></Layout>} />
         <Route path="/Graduation" element={<Layout>< Graduation/></Layout>} />
         <Route path="/adminpage" element={<Layout><AdminPage/></Layout>} /> 
         <Route element={<SuperAdminLayout />}>
--- src/components/schedulespage/Emails.jsx
+++ src/components/schedulespage/Emails.jsx
@@ -109,7 +109,7 @@ const Emails = () => {
 
       {/* New Section: Student List Header */}
       <div className="bg-[#014F9E] p-1 flex justify-between items-center px-4 sm:p-2 sm:px-6">
-        <span className="text-white text-lg leading-none sm:text-xl -mt-2 mb-8">Notices</span>
+        <span className="text-white text-lg leading-none sm:text-xl -mt-2 mb-8">Emails</span>
         <button className="bg-white text-[#014F9E] py-1 px-2 rounded mb-8 flex items-center text-sm leading-none sm:py-2 sm:px-4 sm:text-base mt-0 mb-2">
           <FontAwesomeIcon icon={faPlus} className="mr-1 sm:mr-2" />
           Compose
@@ -121,14 +121,14 @@ const Emails = () => {
         <div className="bg-white rounded-lg shadow-lg mx-4 py-6 px-4">
           <div className="grid grid-cols-6 gap-2">
             <a href="/Schedules" className="flex flex-col sm:flex-row items-center justify-center">
-              <span className="text-blue-600 text-xs sm:text-sm mb-1 mr-2 sm:mb-0">Notices</span>
-              <div className="bg-blue-600 text-white rounded-full h-4 w-4 flex items-center justify-center text-[0.65rem] sm:h-6 sm:w-6 sm:text-xs">
+              <span className="text-[#8E959C]  text-xs sm:text-sm mb-1 mr-2 sm:mb-0">Notices</span>
+              <div className="bg-gray-300 text-[#8E959C] rounded-full h-4 w-4 flex items-center justify-center text-[0.65rem] sm:h-6 sm:w-6 sm:text-xs">
                 12
               </div>
             </a>
             <a href="/Emails" className="flex flex-col sm:flex-row items-center justify-center">
-              <span className="text-[#8E959C] text-xs sm:text-sm mb-1 mr-2 sm:mb-0">Emails</span>
-              <div className="bg-gray-300 text-[#8E959C] rounded-full h-4 w-4 flex items-center justify-center text-[0.65rem] sm:h-6 sm:w-6 sm:text-xs">
+              <span className="text-blue-600 text-xs sm:text-sm mb-1 mr-2 sm:mb-0">Emails</span>
+              <div className="bg-blue-600 text-white rounded-full h-4 w-4 flex items-center justify-center text-[0.65rem] sm:h-6 sm:w-6 sm:text-xs">
                 8
               </div>
             </a>
