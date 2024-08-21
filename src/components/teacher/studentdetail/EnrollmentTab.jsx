2dea87166f741d4ee2e485fb7e13b0ef7c8c1985
Beemhuse <Brightawah94@gmail.com>
2024-08-21 02:54:41 -0700

    feat: enrollment tab completed

--- src/components/teacher/studentdetail/EnrollmentTab.jsx
+++ src/components/teacher/studentdetail/EnrollmentTab.jsx
@@ -1,11 +1,9 @@
 import EnrollmentDropdownList from "./enrollment/EnrollmentDropdownList";
 
-export default function EnrollmentTab  ()  {
+export default function EnrollmentTab() {
   return (
     <div className="md:w-2/3 border rounded-xl p-4 w-full">
-      <h3 className="text-lg font-bold">Enrollment Information</h3>
-<EnrollmentDropdownList />
+      <EnrollmentDropdownList />
     </div>
   );
 }
-
--- src/components/teacher/studentdetail/enrollment/EnrollmentDropdownItem.jsx
+++ src/components/teacher/studentdetail/enrollment/EnrollmentDropdownItem.jsx
@@ -1,94 +1,105 @@
 import { useState } from "react";
 import { IoIosArrowForward } from "react-icons/io";
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
+import { MdCancel } from "react-icons/md";
 
-export default function EnrollmentDropdownItem  ({ year, section, status, subjects, electives })  {
-    const [isOpen, setIsOpen] = useState(false);
-  
-    const handleToggle = () => {
-      setIsOpen(!isOpen);
-    };
-  
-    return (
-      <div className="border-b">
-        <div
-          onClick={handleToggle}
-          className="cursor-pointer flex justify-between items-center p-4 bg-white hover:bg-gray-100 transition-colors rounded-xl"
-        >
-          <div>
-            <h3 className="text-lg font-semibold">{year}</h3>
-            <p className="text-gray-600">{section}</p>
-          </div>
-          <div className="flex items-center space-x-2">
-            <span
-              className={`text-xs font-medium px-3 py-1 rounded-full ${
-                status === 'In progress'
-                  ? 'bg-blue-100 text-blue-600'
-                  : 'bg-gray-100 text-gray-600'
-              }`}
-            >
-              {status}
-            </span>
-            <IoIosArrowForward
-              className={`transition-transform ${
-                isOpen ? 'transform rotate-90' : ''
-              }`}
-            />
-          </div>
+export default function EnrollmentDropdownItem({
+  year,
+  section,
+  status,
+  subjects,
+  electives,
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="border-b text-[#54585C]">
+      <div
+        onClick={handleToggle}
+        className="cursor-pointer flex justify-between items-center p-4 bg-white hover:bg-gray-100 transition-colors rounded-xl"
+      >
+        <div>
+          <h3 className="text-xs font-[500] ">{year}</h3>
+          <p className="text-gray-600 text-sm">{section}</p>
         </div>
-        {isOpen && (
-          <div className="p-4 bg-gray-50 rounded-lg">
-            {subjects.length > 0 && (
-              <div>
-                <h4 className="text-md font-bold">Core Subjects</h4>
-                {subjects.map((subject, index) => (
-                  <div
-                    key={index}
-                    className="flex justify-between items-center py-2"
-                  >
-                    <p>{subject.name}</p>
-                    <span
-                      className={`text-xs font-medium px-3 py-1 rounded-full ${
-                        subject.status === 'Completed'
-                          ? 'bg-blue-100 text-blue-600'
-                          : subject.status === 'In Progress'
-                          ? 'bg-yellow-100 text-yellow-600'
-                          : 'bg-gray-100 text-gray-600'
-                      }`}
-                    >
-                      {subject.status}
-                    </span>
-                  </div>
-                ))}
-              </div>
-            )}
-  
-            {electives.length > 0 && (
-              <div className="mt-4">
-                <h4 className="text-md font-bold">Elective Subjects</h4>
-                {electives.map((elective, index) => (
-                  <div
-                    key={index}
-                    className="flex justify-between items-center py-2"
-                  >
-                    <p>{elective.name}</p>
-                    <span className="text-xs font-medium px-3 py-1 rounded-full bg-blue-100 text-blue-600">
-                      {elective.status}
-                    </span>
-                  </div>
-                ))}
-              </div>
-            )}
-          </div>
-        )}
+        <div className="flex items-center space-x-2">
+          <span
+            className={`text-xs font-medium px-3 py-1 rounded-md ${
+              status === "In progress"
+                ? "bg-blue-100 text-blue-600"
+                : "bg-green-100 text-green-600"
+            }`}
+          >
+            {status}
+          </span>
+          {!isOpen ? (
+            <IoIosArrowForward />
+          ) : (
+            <MdCancel className="text-[#D3DBE3]" />
+          )}
+        </div>
       </div>
-    );
-  }
+      {isOpen && (
+        <div className="p-4 border text-[#54585C] rounded-xl">
+          {subjects.length > 0 && (
+            <div>
+              <h4 className="text-xs font-[500] my-4 text-[#54585C]">
+                Core Subjects
+              </h4>
+              {subjects.map((subject, index) => (
+                <div
+                  key={index}
+                  className="flex justify-between border-b   border-0 items-center py-4"
+                >
+                  <p className="text-[#54585C]">{subject.name}</p>
+                  <span
+                    className={`text-sm font-medium px-3 py-1 rounded-md ${
+                      subject.status === "Completed"
+                        ? "bg-blue-100 text-blue-600"
+                        : subject.status === "In Progress"
+                        ? "bg-yellow-100 text-yellow-600"
+                        : "bg-gray-100 text-gray-600"
+                    }`}
+                  >
+                    {subject.status}
+                  </span>
+                </div>
+              ))}
+            </div>
+          )}
 
-  EnrollmentDropdownItem.propTypes = {
-    year: PropTypes.string.isRequired,
-    section: PropTypes.string.isRequired,
-    status: PropTypes.string.isRequired,
-    subjects: PropTypes.arrayOf(PropTypes.object),
-    electives: PropTypes.arrayOf(PropTypes.object),
-  }
+          {electives.length > 0 && (
+            <div className="mt-4">
+              <h4 className="text-xs my-4 font-[500] text-[#54585C]">
+                Elective Subjects
+              </h4>
+              {electives.map((elective, index) => (
+                <div
+                  key={index}
+                  className="flex justify-between border-b border-0 items-center py-4"
+                >
+                  <p>{elective.name}</p>
+                  <span className="text-xs font-medium px-3 py-1 rounded-md bg-blue-100 text-blue-600">
+                    {elective.status}
+                  </span>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
+
+EnrollmentDropdownItem.propTypes = {
+  year: PropTypes.string.isRequired,
+  section: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired,
+  subjects: PropTypes.arrayOf(PropTypes.object),
+  electives: PropTypes.arrayOf(PropTypes.object),
+};
